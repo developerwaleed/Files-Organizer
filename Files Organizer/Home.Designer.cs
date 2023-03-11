57bbd0015c4280a04a3b08eb03b666d07ea7b554
Waleed Amjad <waleedamjad99@hotmail.com>
2023-03-11 22:27:58 +0500

    Add UI for creating cutom folder

--- Files Organizer/Home.Designer.cs
+++ Files Organizer/Home.Designer.cs
@@ -34,13 +34,23 @@
             this.applyBtn = new System.Windows.Forms.Button();
             this.dirName = new System.Windows.Forms.Label();
             this.dir = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(69, 55);
+            this.label1.Location = new System.Drawing.Point(85, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(235, 39);
             this.label1.TabIndex = 0;
@@ -51,7 +61,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(21, 143);
+            this.label2.Location = new System.Drawing.Point(37, 403);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(279, 20);
             this.label2.TabIndex = 1;
@@ -59,7 +69,7 @@
             // 
             // browseBtn
             // 
-            this.browseBtn.Location = new System.Drawing.Point(306, 143);
+            this.browseBtn.Location = new System.Drawing.Point(322, 403);
             this.browseBtn.Name = "browseBtn";
             this.browseBtn.Size = new System.Drawing.Size(75, 23);
             this.browseBtn.TabIndex = 2;
@@ -69,7 +79,7 @@
             // 
             // applyBtn
             // 
-            this.applyBtn.Location = new System.Drawing.Point(137, 219);
+            this.applyBtn.Location = new System.Drawing.Point(153, 479);
             this.applyBtn.Name = "applyBtn";
             this.applyBtn.Size = new System.Drawing.Size(109, 33);
             this.applyBtn.TabIndex = 3;
@@ -80,7 +90,7 @@
             // dirName
             // 
             this.dirName.AutoSize = true;
-            this.dirName.Location = new System.Drawing.Point(124, 182);
+            this.dirName.Location = new System.Drawing.Point(140, 442);
             this.dirName.Name = "dirName";
             this.dirName.Size = new System.Drawing.Size(111, 13);
             this.dirName.TabIndex = 4;
@@ -90,20 +100,113 @@
             // dir
             // 
             this.dir.AutoSize = true;
-            this.dir.Location = new System.Drawing.Point(73, 182);
+            this.dir.Location = new System.Drawing.Point(89, 442);
             this.dir.Name = "dir";
             this.dir.Size = new System.Drawing.Size(52, 13);
             this.dir.TabIndex = 5;
             this.dir.Text = "Directory:";
             this.dir.Visible = false;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(28, 84);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Custom Folders?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(133, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(266, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "(If not selecte Folder will be created by extention name)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(352, 191);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Add Folder Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 144);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(235, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.applyBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(128, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Extension Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(247, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Folder Name";
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 328);
+            this.ClientSize = new System.Drawing.Size(442, 564);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.dir);
             this.Controls.Add(this.dirName);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.applyBtn);
             this.Controls.Add(this.browseBtn);
             this.Controls.Add(this.label2);
@@ -111,6 +214,7 @@
             this.Name = "Home";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Home_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,6 +228,15 @@
         private System.Windows.Forms.Button applyBtn;
         private System.Windows.Forms.Label dirName;
         private System.Windows.Forms.Label dir;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
