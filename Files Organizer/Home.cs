8c47f5e1c1e095901a42aca1e9c2f416c68be2ab
Waleed Amjad <waleedamjad99@hotmail.com>
2023-03-11 21:39:24 +0500

    Add browse folder code

---
+++ Files Organizer/Home.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Files_Organizer
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void applyBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            using (var folderBrowserDialog1 = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+                if(result == DialogResult.OK)
+                {
+                dirName.Text = folderBrowserDialog1.SelectedPath;
+                dirName.Visible= true;
+                dir.Visible= true;
+                }
+                else
+                {
+                    MessageBox.Show("Something Went Wrong Try Again Please!");
+                }
+            }
+
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
